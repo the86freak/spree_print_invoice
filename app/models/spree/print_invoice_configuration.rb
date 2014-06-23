7815605377ca90c1ce4393d2671d39102b144f42
Malte Horstmann <horstmann@wi1.uni-hohenheim.de>
2014-06-23 17:02:07 +0200

    adjusted config

--- app/models/spree/print_invoice_configuration.rb
+++ app/models/spree/print_invoice_configuration.rb
@@ -2,15 +2,22 @@ module Spree
   class PrintInvoiceConfiguration < Preferences::Configuration
 
     preference :print_invoice_next_number, :integer, :default => nil
-    preference :print_invoice_logo_path, :string, :default => Spree::Config[:admin_interface_logo]
+#    preference :print_invoice_logo_path, :string, :default => Spree::Config[:admin_interface_logo]
     preference :print_invoice_logo_scale, :integer, :default => 50
     preference :print_invoice_font_face, :string, :default => 'Helvetica'
     preference :print_buttons, :string, :default => 'invoice'
-    preference :prawn_options, :hash, :default => {}
-    preference :print_invoice_footer_right1, :string, :default => 'Website:'
-    preference :print_invoice_footer_right2, :string, :default => 'http://www.google.com'
-    preference :print_invoice_footer_left1, :string, :default => 'Phone Number:'
-    preference :print_invoice_footer_left2, :string, :default => '+48-664-446-595'
+
+#    preference :prawn_options, :hash, :default => {}
+#    preference :print_invoice_footer_right1, :string, :default => 'Website:'
+#    preference :print_invoice_footer_right2, :string, :default => 'http://www.google.com'
+#    preference :print_invoice_footer_left1, :string, :default => 'Phone Number:'
+#    preference :print_invoice_footer_left2, :string, :default => '+48-664-446-595'
+
+
+    preference :print_invoice_logo_path => "logo.png"
+#    preference :print_invoice_next_number => 24
+    preference :prawn_options => {:page_layout => :portrait, :page_size => "A4", :margin => [50, 50, 50, 50]})
+
 
     def use_sequential_number?
       print_invoice_next_number.present? && print_invoice_next_number > 0
